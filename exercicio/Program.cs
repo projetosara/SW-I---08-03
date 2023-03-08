2229aaac025f71642a6984899ed20dd30f43ae94
projetosara <100379644+projetosara@users.noreply.github.com>
2023-03-08 16:59:01 -0300

    Add files via upload

---
+++ exercicio/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace exercicio;
+class Program
+{
+    static void Main(string[] args)
+    {
+       //Console.WriteLine("Hello, World!");
+        //Console.ReadKey();
+
+        Exemplo exe01 = new Exemplo();
+
+        exe01.mostramsg();
+
+        exe01.pegaNome("SARA");
+
+        Console.WriteLine(exe01.mensagem());
+
+        Console.WriteLine("O resultado da soma é:" + exe01.somar(15,15));
+    }
+}
